7dd7103f15a6e877c8736dfb43a2751e67fae3d8
ReighnZ <reighnzenric@gmail.com>
2023-09-27 10:41:43 +0400

    Update Exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 1.py
@@ -1,6 +1,6 @@
 # The first exercise is a Python code prints out the lyrics of the nursery rhyme "Twinkle, Twinkle, Little Star" using multiline string and the print statement.
-#The print statement is used to display text in the console.
-#The text being printed is enclosed within triple double-quote """ characters. This allows us to create a multiline string, which means we can write multiple lines of text without having to use separate print statements for each line.
+# The print statement is used to display text in the console.
+# The text being printed is enclosed within triple double-quote """ characters. This allows us to create a multiline string, which means we can write multiple lines of text without having to use separate print statements for each line.
 
 print("""Twinkle, twinkle, little star,
     How I wonder what you are!
