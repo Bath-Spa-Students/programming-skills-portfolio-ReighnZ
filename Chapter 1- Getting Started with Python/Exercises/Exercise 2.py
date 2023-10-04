dc1791732e649ebbabdd503de9b770970ed4b30b
ReighnZ <reighnzenric@gmail.com>
2023-10-04 13:24:06 +0400

    Exercise 2.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 2.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 2.py
@@ -1,6 +1,8 @@
-# The sys module is imported, giving access to a number of system-specific parameters and functions.
-# The information about the Python version is retrieved via the sys.version property.
-# The Python version you are using is then shown in a message that is printed to the console.
+''' The sys module is imported, giving access to a number of system-specific parameters and functions.
+
+The information about the Python version is retrieved via the sys.version property.
+
+The Python version you are using is then shown in a message that is printed to the console. '''
 
 import sys
 python_version = sys.version
