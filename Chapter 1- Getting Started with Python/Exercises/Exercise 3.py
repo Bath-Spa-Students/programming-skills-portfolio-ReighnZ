f34ba8f0370dd8e3f2c4518dff28b2009362ccc9
ReighnZ <reighnzenric@gmail.com>
2023-10-04 13:24:21 +0400

    Exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
@@ -1,7 +1,10 @@
-# The datetime module is imported, offering classes and functions for managing dates and times.
-# To get the current date and time it uses the datetime.datetime.now() function.
-# Using the strftime method, it formats the current date and time as a string. It is structured as "Year-Month-Day Hour:Minute:Second" in this instance.
-# The formatted date and time are then printed to the console.
+''' The datetime module is imported, offering classes and functions for managing dates and times.
+
+To get the current date and time it uses the datetime.datetime.now() function.
+
+Using the strftime method, it formats the current date and time as a string. It is structured as "Year-Month-Day Hour:Minute:Second" in this instance.
+
+The formatted date and time are then printed to the console. '''
 
 import datetime
 now = datetime.datetime.now()
