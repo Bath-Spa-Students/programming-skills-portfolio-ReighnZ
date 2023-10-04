14ef8282066396a60dc7b3850b3e111aeddf166c
ReighnZ <reighnzenric@gmail.com>
2023-10-04 10:35:01 +0400

    Exercise 1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Pyhton File.py
@@ -0,0 +1,38 @@
+print ( "Reighn" , "Zenric")
+
+name = "Reighn"
+name = "Zenric"
+print (name)
+
+name1 = "Khadijah"
+name2 = "Lloyd"
+print (name1 , name2)
+
+print (14/2)
+
+print (14//2)
+
+name = input ("Enter Name: ")
+print (name)
+
+age = input ("Enter your age: ")
+print (age)
+
+'''Hi instead of using a lot of #
+use this to make multiple lines'''
+
+'''Python operator precedence:
+Operations enclosed in parentheses
+Forces operations to be performed before others
+Exponentiation (**)
+Multiplication (*)
+Division (/ and //)
+Remainder (%)
+Addition (+)
+Substraction (-)
+Higher precedence performed first
+Same precedence operators execute from left to right
+'''
+
+ans = ((2*3/8+(23+5)))
+print ((ans))
