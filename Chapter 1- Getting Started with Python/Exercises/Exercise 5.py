2dc575aa2719f519b7d1de2c7937f55ad63afb9a
ReighnZ <reighnzenric@gmail.com>
2023-10-10 23:58:07 +0400

    Exercise 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 5.py
@@ -0,0 +1,14 @@
+'''The program requests the user to provide the radius of a circle, presenting the message "Enter the radius of the circle:".
+
+User input is obtained through the input() function, which captures the user's response as a string.
+
+To facilitate mathematical operations, the program converts the user's input (originally a string)
+into a floating-point number using float().
+
+The program computes the circle's area using the formula: area = π * radius^2, where π (pi) is approximately 3.14159265359.
+
+The calculated area is presented to the user, along with the message "The area of the circle is:", followed by the computed value.'''
+
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14159265359 * (radius ** 2)
+print("The area of the circle is:", area)
