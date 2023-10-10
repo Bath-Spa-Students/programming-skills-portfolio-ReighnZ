d18f3374b22fbfd4b83ec063e8f36b2e50b4b4ea
ReighnZ <reighnzenric@gmail.com>
2023-10-10 23:51:17 +0400

    Exercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
@@ -0,0 +1,14 @@
+'''This code creates three different text strings and stores them in variables:
+tring_1 contains "Hello, ".
+string_2 contains "Welcome to ".
+string_3 contains "Code Lab 1."
+
+It then combines these three strings together using the + operator and stores the result in a new variable called string_4.
+
+It then prints the value of string_4 to the screen.'''
+
+string_1 = "Hello, "
+string_2 = "Welcome to "
+string_3 = "Code Lab 1."
+string_4 = string_1 + string_2 + string_3
+print(string_4)
