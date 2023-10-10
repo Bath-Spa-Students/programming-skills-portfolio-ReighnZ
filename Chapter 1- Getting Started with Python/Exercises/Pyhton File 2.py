a42e7b93020e1cc8fb2cf91578de8822adedfb0d
ReighnZ <reighnzenric@gmail.com>
2023-10-10 23:51:33 +0400

    Python File 2.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Pyhton File 2.py
@@ -0,0 +1,36 @@
+'''string_1 = "Reighn"
+string_2 = " Zenric"
+string_3 = string_1 + string_2
+print (string_3, end="\n")
+print ("Hello welcome to code lab 1", end="\t")
+print ("It is October 10")'''
+
+'''end= is a delimiter that allows us to print strings in the same line
+/n adds a new line
+/t adds space or tab between strings
+'''
+
+'''string_1 = "Reighn"
+string_2 = " Zenric"
+print (f'The value is {string_1 + string_2}.')
+
+print (f'The value is {10+2}.')'''
+
+import turtle
+turtle.forward (100)
+turtle.right (90)
+turtle.forward (100)
+turtle.right (90)
+turtle.forward (100)
+turtle.right (90)
+turtle.forward (150)
+turtle.right (90)
+turtle.forward (150)
+turtle.right (90)
+turtle.forward (200)
+turtle.right (90)
+turtle.forward (200)
+turtle.right (90)
+turtle.forward (200)
+turtle.right (90)
+turtle.forward (55)
