27c5cf2675f070749d2654a6b0024ce7faa08c96
ReighnZ <reighnzenric@gmail.com>
2023-10-23 20:37:56 +0400

    Exercise 4.py

--- Chapter 3- Data Structures/Exercises/Exercise 4.py
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -1,4 +1,4 @@
-''' This code creates a list of three individuals' names and uses a for loop to print personalized dinner invitations to each person.
+''' This code creates a list of three individuals' names and uses a 'for' loop to print personalized dinner invitations to each person.
 Each invitation message includes the person's name and a request to join the dinner. '''
 
 guests = ["John", "Lloyd", "Khadijah"]
