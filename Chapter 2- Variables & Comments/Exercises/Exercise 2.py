4e14a17072a4d2861159e305110f4c30ac05e76e
ReighnZ <reighnzenric@gmail.com>
2023-10-23 19:50:51 +0400

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+''' In this code, a quote by Taylor Swift is assigned to the variable quote, and the author's name, Taylor Swift, is assigned to the variable author.
+Using an f-string, the code combines these variables and presents them as a sentence, with the quote enclosed in quotation marks, attributing it to the author.
+When executed, the code prints the combined sentence, effectively showcasing a quote from Taylor Swift. '''
+
+quote = "Just be yourself, there is no one better."
+author = "Taylor Swift"
+print(f"{author} once said, \"{quote}\"")
