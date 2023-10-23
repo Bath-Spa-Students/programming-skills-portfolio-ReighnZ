44b4174b12d435d22e34e9c813b8ffb81b4032ca
ReighnZ <reighnzenric@gmail.com>
2023-10-23 20:16:12 +0400

    Exercise 1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 1.py
@@ -0,0 +1,6 @@
+''' The code defines a list called names containing the names of four individuals.
+It then utilizes a for loop to print each name from the list on a separate line, displaying them sequentially. '''
+
+names = ["John", "Lloyd", "Khadijah", "Ifrah"]
+for name in names:
+    print(name)
