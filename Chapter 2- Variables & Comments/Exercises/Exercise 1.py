18e3e1c8fbb9c8128cf3f8012c8113d7d5639215
ReighnZ <reighnzenric@gmail.com>
2023-10-23 19:31:52 +0400

    Exercise 1.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 1.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -1,4 +1,6 @@
-''''''
+''' The code begins by assigning the message "Hello, world!" to a variable called message.
+It then prints this initial message and subsequently updates the message variable with the content "Hi, world!" before printing the new message.
+Basically, it starts with one message, prints it, changes the message, and then prints the updated message. '''
 
 message = "Hello, world!"
 print("Initial message:", message)
