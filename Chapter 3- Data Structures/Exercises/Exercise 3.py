2d10dfb490228c4034b95823baf67c03414ac7d8
ReighnZ <reighnzenric@gmail.com>
2023-10-23 20:30:04 +0400

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,7 @@
+''' The code contains a list named transportation_modes with various modes of transportation.
+It uses a for loop to print personalized statements for each mode, expressing a desire to own each one.
+The printed statements convey a wish to possess these modes of transportation. '''
+
+transportation_modes = ["car", "motorcycle", "jet ski", "private jet", "bicycle"]
+for mode in transportation_modes:
+    print(f"I would like to own a {mode}.")
