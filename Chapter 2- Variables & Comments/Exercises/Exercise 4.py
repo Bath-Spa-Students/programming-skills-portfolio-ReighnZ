559ae10c649a2fe3cffe2e558cd39148ce997883
ReighnZ <reighnzenric@gmail.com>
2023-10-23 20:07:30 +0400

    Exercise 4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -0,0 +1,4 @@
+#The code assigns my favorite number which is 6 to a variable and then prints a message revealing that favorite number.
+
+favorite_number = 6
+print("My favorite number is", favorite_number)
