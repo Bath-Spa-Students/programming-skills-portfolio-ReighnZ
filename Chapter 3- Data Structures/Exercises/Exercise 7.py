9458745aba31c9f20071003a7f3a473ddb18dcde
ReighnZ <reighnzenric@gmail.com>
2023-10-24 23:38:32 +0400

    Exercise 7.py

--- Chapter 3- Data Structures/Exercises/Exercise 7.py
+++ Chapter 3- Data Structures/Exercises/Exercise 7.py
@@ -1,5 +1,5 @@
 ''' This code starts with a list of places to visit and demonstrates various operations on this list.
-It includes sorting the list in alphabetical and reverse alphabetical order using sorted(), reversing the list using reverse(), and sorting it in place using sort().
+It includes sorting the list in alphabetical and reverse alphabetical order using 'sorted()', reversing the list using 'reverse()', and sorting it in place using 'sort()'.
 It confirms that the original list order is preserved when these operations are applied and shows the results of each operation. '''
 
 places_to_visit = ["New York", "Tokyo", "Japan", "London", "South Korea"]
