700b36adeb4e7a51ae0c8bdd6f08543aa374615e
ReighnZ <reighnzenric@gmail.com>
2023-10-30 18:19:59 +0400

    Class Practice.py

--- Chapter 4- Control Flow/Exercises/Class Practice.py
+++ Chapter 4- Control Flow/Exercises/Class Practice.py
@@ -6,7 +6,7 @@ else:
     item = "Affordable"
 print("Your item : "+str(item))'''
 
-# Nested Decision Structure
+'''# Nested Decision Structure
 price = int(input("Enter price of item : "))
 calories = float(input("Enter calories of item : "))
 
@@ -16,4 +16,18 @@ if price <=1000:
  else :
         print("The item is affordable but not healthy.")
 else :
-        print("The item is expensive.")
+        print("The item is expensive.")'''
+
+percentage = float (input("Enter the calories of your item : "))
+if percentage >= 2000 :
+    print("Your item is too heavy of a meal.")
+elif percentage >= 1500 :
+        print("Your item is a heavy meal")
+elif percentage >= 1000 :
+        print("Your item is a average meal.")
+elif percentage >= 500 :
+        print("Your item is a light meal.")
+elif percentage >= 100 :
+        print("Your item is a snack.")
+else :
+       print("Your item is too small.")
