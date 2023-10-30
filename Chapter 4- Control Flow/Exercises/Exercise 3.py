9062b86148cebf564f634a7afb08c37829d6c5be
ReighnZ <reighnzenric@gmail.com>
2023-10-30 20:24:35 +0400

    Exercise 3.py

--- Chapter 4- Control Flow/Exercises/Exercise 3.py
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -1,5 +1,5 @@
 ''' This code provides three versions, each with a different color for an alien.
-It uses an if-elif-else chain to determine the color of the alien and print a corresponding message about the points the player earns for shooting that alien.
+It uses an 'if-elif-else chain' to determine the color of the alien and print a corresponding message about the points the player earns for shooting that alien.
 The points vary based on the color: 5 points for green, 10 points for yellow, and 15 points for red. '''
 
 # Version 1
