8e5d56ed8d4e4bc60212a0c87fdce5beffdb4ff1
ReighnZ <reighnzenric@gmail.com>
2023-10-30 18:26:02 +0400

    Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -0,0 +1,13 @@
+''' This code demonstrates conditional statements in two versions. In the first version, it checks if an alien's color is 'green' and prints a message if true.
+In the second version, the alien's color is changed to 'red', and the code doesn't print any message because the condition is not met.
+It illustrates how conditions are used to make decisions based on variable values. '''
+
+# Version 1
+alien_color = 'green'
+if alien_color == 'green':
+    print("The player just earned 5 points.")
+
+# Version 2
+alien_color = 'red'
+if alien_color == 'green':
+    print("The player just earned 5 points.")
