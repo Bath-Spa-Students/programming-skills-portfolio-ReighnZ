98d67f50fc650867f02077635b0985da8bb02e6f
ReighnZ <reighnzenric@gmail.com>
2023-10-30 20:13:49 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,15 @@
+''' This code defines a dictionary called 'person_info' that holds information about an individual, including their first name, last name, age, and city.
+It then proceeds to print each piece of data by accessing the corresponding keys in the dictionary.
+This code serves as an example of using dictionaries to manage and display personal information efficiently. '''
+
+person_info = {
+    "first_name": "Edward",
+    "last_name": "Winston",
+    "age": 20,
+    "city": "Manchester"
+}
+
+print("First Name:", person_info["first_name"])
+print("Last Name:", person_info["last_name"])
+print("Age:", person_info["age"])
+print("City:", person_info["city"])
