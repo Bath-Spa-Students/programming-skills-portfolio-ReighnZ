a00b682fa99ae167a1c95124d0d2dd9da8a72608
ReighnZ <reighnzenric@gmail.com>
2023-10-30 18:30:59 +0400

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,21 @@
+''' This code provides two versions, each with a different value for the alien_color variable.
+In the first version, where alien_color is 'green', the code prints a message stating that the player earned 5 points.
+In the second version, where alien_color is 'red', the code prints a message indicating that the player earned 10 points.
+These versions demonstrate how the program's response changes based on the value of alien_color. '''
+
+# Version 1
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("The player just earned 5 points for shooting the alien.")
+else:
+    print("The player just earned 10 points.")
+
+# Version 2
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("The player just earned 5 points for shooting the alien.")
+else:
+    print("The player just earned 10 points.")
+
