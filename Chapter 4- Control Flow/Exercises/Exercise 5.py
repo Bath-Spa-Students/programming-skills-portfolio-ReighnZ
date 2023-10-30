accf0946e6eb83009ec810b8a8fbfe879b26a693
ReighnZ <reighnzenric@gmail.com>
2023-10-30 18:49:03 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,20 @@
+''' This code checks if specific fruits (mango, banana, and orange) are in a list of favorite fruits.
+If a fruit is found in the list, it prints a statement indicating that the person really likes that fruit.
+Fruits not found in the list are not considered. '''
+
+favorite_fruits = ["mango", "banana", "orange"]
+
+if "mango" in favorite_fruits:
+    print("You really like mangoes!")
+
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
+
+if "orange" in favorite_fruits:
+    print("You really like oranges!")
+
+if "strawberry" in favorite_fruits:
+    print("You really like strawberries!")
+
+if "apple" in favorite_fruits:
+    print("You really like apples!")
