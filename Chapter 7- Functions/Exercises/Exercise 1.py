949d81654590f64d9d23bf9474b4060cf0e921af
ReighnZ <reighnzenric@gmail.com>
2023-11-05 17:23:35 +0400

    Exercise 1.py

---
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -0,0 +1,8 @@
+''' This code defines a Python function called 'display_message' that prints a specific message.
+When this function is called, it displays a message about the topic you are learning in the chapter, which is related to Python functions. '''
+
+def display_message():
+    message = "In this chapter, I'm learning about functions."
+    print(message)
+
+display_message()
