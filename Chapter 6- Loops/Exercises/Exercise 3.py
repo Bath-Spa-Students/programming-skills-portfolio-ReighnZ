a71e234e2e189052197a9b179a992664e79ef1c0
ReighnZ <reighnzenric@gmail.com>
2023-11-05 16:57:49 +0400

    Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,6 +1,6 @@
 ''' This code creates an infinite loop using 'while True'. This loop will run indefinitely until manually stopped.
 It includes a 'pass' statement, which acts as a placeholder, ensuring the loop has some content.
-To exit the loop, you need to interrupt its execution, such as by pressing Ctrl-C or closing the program or terminal.'''
+To exit the loop, you need to interrupt its execution, such as by pressing Ctrl-C or closing the program or terminal. '''
 
 while True:
     pass
