806c0507c34e3b63c72a8987325ad0810e9409f7
ReighnZ <reighnzenric@gmail.com>
2023-11-05 17:31:45 +0400

    Exercise 2.py

---
+++ Chapter 7- Functions/Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+''' This code defines a function called 'favorite_book(title)' that takes a book title as an argument and prints a message stating that it's one of the favorite books.
+The function is then called with the argument "The Hunger Games," resulting in a message being printed for that specific book. '''
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+favorite_book("The Hunger Games")
